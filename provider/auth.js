2a8d693938c2bc96f56d92cf69f19c55867bdeec
Jason Walsh <j@wal.sh>
2016-12-19 23:47:47 -0500

    Separating multiple JWT implementations

--- provider/auth.js
+++ provider/auth.js
@@ -18,9 +18,12 @@ import {
 
 const prefix = (new Array(40)).join('-');
 
-// Crypto https://nodejs.org/api/crypto.html
+// Crypto
+// https://nodejs.org/api/crypto.html
 console.log(prefix, 'crypto');
 const prime_length = 60;
+// Alternatively look at the implementation for
+// https://en.wikipedia.org/wiki/Elliptic_curve_Diffie%E2%80%93Hellman
 const diffHell = crypto.createDiffieHellman(prime_length);
 
 diffHell.generateKeys('base64');
@@ -32,6 +35,8 @@ console.log("Public Key : ", rawPublicKey);
 console.log("Private Key : ", rawPrivateKey);
 
 // JSON Web Tokens (JWTs)
+// This is the Blockstack implementation of the specification
+// http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html
 console.log(prefix, 'jwt');
 
 
@@ -45,6 +50,9 @@ console.log(token);
 
 const tokenData = decodeToken(token);
 console.log(tokenData);
+
+// The canonical implmenetation is provided through
+// https://github.com/auth0/node-jsonwebtoken
 
 // Signing
 console.log(prefix, 'signing');
