5f92ea2109ae06bc2ca6abb22fffc05d61d4ee4b
Jason Walsh <j@wal.sh>
2016-12-20 16:49:36 -0800

    npm start
    Provides a simple wrapper for starting all three services.

--- resolver/index.js
+++ resolver/index.js
@@ -1,7 +1,18 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var http = require('http');
 
 app.get('/', function (req, res) {
+  var query = req.query;
+  console.log(query);
+
+  http.get({
+        host: query.callback_url,
+        path: '/' + query.id
+  }, function(response) {
+    console.log(response);
+  });
+  // Look up the provider and queue the signing request
   res.send({
 	id: "akscanb.id",
 	digitally_signed_payload: "XXXXXXXXXXXXXXXXX"
