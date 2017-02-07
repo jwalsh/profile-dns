5f0d08fa3450c795d845183715691bb589b7d1fd
Jason Walsh <j@wal.sh>
2017-02-06 19:21:02 -0500

    Scaffolding for React and RxJS

--- promissory-note/src/main.js
+++ promissory-note/src/main.js
@@ -1,13 +1,40 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint-disable no-new no-console*/
+
+import Immutable from 'immutable';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactMarkdown from 'react-markdown';
+import Rx from 'rxjs/Rx';
 import Vue from 'vue';
+
 import App from './App';
 import router from './router';
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App },
-});
+
+
+// const map1 = Immutable.Map({a:1, b:2, c:3});
+// console.log(map1.get('b'));
+
+// Rx.Observable
+//   .of('Promissory Note')
+//   .subscribe((x) => { return x; });
+
+
+// var input = '# This is a header\n\nAnd this is a paragraph';
+
+// ReactDOM.render(
+//     <ReactMarkdown source={input} />,
+//     document.getElementById('container')
+// );
+
+// ReactDOM.render(
+//   <h1>Promissory Notes</h1>,
+//   document.getElementById('root')
+// );
+
+// const view = new Vue({
+//   el: '#app',
+//   router,
+//   template: '<App/>',
+//   components: { App },
+// });
