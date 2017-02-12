08c496569421656b16f5e5d442ac43d91a4bbbbf
Jason Walsh <j@wal.sh>
2017-02-11 20:21:36 -0500

    Mocking for loan applications

---
+++ examiner-tools/loan-appraisal-report.py
@@ -0,0 +1,3 @@
+# This is a written report that estimates the current fair market value of the property that you are buying or selling.
+# https://www.equitynationaltitle.com/
+# Input: Address, Loan Application
---
+++ examiner-tools/loan-initial-dislosure.py
@@ -0,0 +1,1 @@
+# http://homeguides.sfgate.com/mortgages-initial-disclosure-rules-100580.html
