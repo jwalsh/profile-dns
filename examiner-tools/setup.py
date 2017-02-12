bde583ada1d381248b94e2822998d2b19eeacf06
Jason Walsh <j@wal.sh>
2017-02-11 20:15:36 -0500

    Examiner tools scaffolding

---
+++ examiner-tools/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="examiner-tools",
+    version="0.1.0",
+    description="Examiner tools",
+    license="MIT",
+    author="jwalsh",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ]
+)
