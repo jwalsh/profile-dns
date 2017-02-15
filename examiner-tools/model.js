6d4a3c5b75e43b7034f299271f4824634ab401d9
Jason Walsh <j@wal.sh>
2017-02-15 08:03:06 -0500

    Simulate events

--- examiner-tools/model.js
+++ examiner-tools/model.js
@@ -1,19 +1,20 @@
 // Summarize actions
 // days: days in state
 // accept: rate
-var _ = loanEntityStates = {
+// var _ require('lodash');
+var $ = {
   MEMBER: {
     APPLICATION: {
       description: 'Create loan application',
       accept: 1,
       days: 0,
-      next: [_.MORTGAGE_ADMINISTRATOR.VET_LOAN]
+      next: ['MORTGAGE_ADMINISTRATOR.VET_LOAN']
     },
     FEEDBACK: {
       description: 'Give feedback  loan application',
       accept: 1,
       days: 0,
-      next: [_.MORTGAGE_ADMINISTRATOR.VET_LOAN]
+      next: ['MORTGAGE_ADMINISTRATOR.VET_LOAN']
     }
   },
 
@@ -22,7 +23,7 @@ var _ = loanEntityStates = {
       description: 'Mortgage administrator vets / scrubs inbound loan applications: ACCEPT: 50%',
       accept: .5,
       days: 1,
-      next: [_.MORTGAGE_PROCESSOR.DASHBOARD_REVIEW]
+      next: ['MORTGAGE_PROCESSOR.DASHBOARD_REVIEW']
     }
   },
 
@@ -31,25 +32,25 @@ var _ = loanEntityStates = {
       description: 'Mortgage processor reviews dashboard with new loan',
       accept: 1,
       days: 1,
-      next: [_.MORTGAGE_PROCESSOR.ACCEPT_LOAN, _.MEMBER.FEEDBACK]
+      next: ['MORTGAGE_PROCESSOR.ACCEPT_LOAN','MEMBER.FEEDBACK']
     },
     ACCEPT_LOAN: {
       description: 'Mortgage processor vets loan application: ACCEPT: 90%',
       accept: .9,
       days: 1.5,
-      next: [_.MORTGAGE_PROCESSOR.LOAN_ESTIMATE]
+      next: ['MORTGAGE_PROCESSOR.LOAN_ESTIMATE']
     },
     LOAN_ESTIMATE: {
       description: 'Mortgage processor creates loan estimate (see loan-estimate.py)',
       accept: 1,
       days: 0,
-      next: [_.APPRAISER.REQUEST, _.TITLE_EXAMINER.REQUEST]
+      next: ['APPRAISER.REQUEST','TITLE_EXAMINER.REQUEST']
     },
     PROMISSORY_NOTE: {
       description: 'Create terms of the agreement ',
       accept: .8,
       days: 0,
-      next: [_.LENDER.PROMISSORY_NOTE]
+      next: ['LENDER.PROMISSORY_NOTE']
     }
   },
 
@@ -58,19 +59,19 @@ var _ = loanEntityStates = {
       description: 'Initialize the appraisal',
       accept: 1,
       days: 0,
-      next: [_.APPRAISER.HOLD, _.APPRAISER.APPRAISAL_REPORT]
+      next: ['APPRAISER.HOLD','APPRAISER.APPRAISAL_REPORT']
     },
     HOLD: {
       description: 'Appraiser holds appraisal: HOLD: 10%',
       accept: .1,
       days: 0,
-      next: [_.APPRAISER.HOLD_NOTIFICATION]
+      next: ['APPRAISER.HOLD_NOTIFICATION']
     },
     HOLD_NOTIFICATION: {
       description: 'Notify the mortgage processor of held loan: NOTIFY: 100%',
       accept: 1,
       days: 2,
-      next: [_.APPRAISER.APPRAISAL_REPORT]
+      next: ['APPRAISER.APPRAISAL_REPORT']
     },
     APPRAISAL_REPORT: {
       description: 'Notify the mortgage processor of held loan: NOTIFY: 100%',
@@ -106,8 +107,14 @@ var _ = loanEntityStates = {
   }
 };
 
-
-
-var loanStates = {
-  // MEMBER['CREATE_LOAN']: MORTGAGE_ADMINISTRATOR['VET_LOAN']
+var step = function(next) {
+  return next[0];
 };
+
+var start = $.MEMBER.APPLICATION;
+var action = start;
+while (action.next) {
+  console.log(action);
+  var s = action.next[0].split('.');
+  action = $[s[0]][s[1]];
+}
