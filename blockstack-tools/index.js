30bc2251e097172bf7a1b117bf133ae53f8eb211
Jason Walsh <j@wal.sh>
2017-02-23 19:57:04 -0500

    Evaluating eccrypto
    Run examples with
    
        npm test

--- blockstack-tools/index.js
+++ blockstack-tools/index.js
@@ -1,5 +1,7 @@
+// Example: elliptic
+
 import elliptic from 'elliptic'
-// console.log(elliptic)
+console.log('elliptic', Object.keys(elliptic))
 
 import wallet from './wallet.json'
 // console.log(wallet)
@@ -18,9 +20,35 @@ let pub = ec.keyFromPrivate(pubkey, 'hex')
 let msg = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 
 let signature = priv.sign(msg)
-console.log(signature)
+// console.log(signature)
 
 // Export DER encoded signature in Array
 let derSign = signature.toDER()
+console.log('elliptic: message verification', priv.verify(msg, derSign))
 
-console.log(priv.verify(msg, derSign))
+
+// Example: opengpg
+// import openpgp from 'openpgp'
+import ecdsa from 'secp256k1'
+import crypto from 'crypto'
+console.log('ecdsa', Object.keys(ecdsa))
+
+// let privateKey = crypto.randomBytes(32)
+//a random message to sign
+let rmsg = crypto.randomBytes(32)
+
+//get the public key in a compressed format
+// let pubKey = ecdsa.createPublicKey(privkey, true)
+
+//sign the message
+// let sig = ecdsa.sign(rmsg, privkey)
+
+//verify the signature
+// if(ecdsa.verify(msg, sig, pubKey)){
+//   console.log("valid signature")
+// }
+
+
+import eccrypto from 'eccrypto'
+console.log('eccrypto', Object.keys(eccrypto))
+let pkey = eccrypto
